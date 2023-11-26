6416d223020ce4b99b369abbf2c5c818123449a5
erikakr <erika.kriksciunaite@nordsec.com>
2023-11-26 18:43:20 +0200

    Modifying old funcs file.

--- functions.cpp
+++ functions.cpp
@@ -31,10 +31,63 @@ using std::ofstream;
 using std::left;
 using std::setw;
 
+// NEW
+
+float calculateFinalMarkAvg(const Student &student) {
+    float sum = 0.0;
+
+    // Retrieve the marks using the getter function
+    vector<int> marks = student.get_marks();
+
+    if (marks.empty()) {
+        return 0.6 * student.get_examMark(); // If no individual marks, return 0.6 * exam mark
+    }
+    
+    // Calculate the sum of individual marks
+    for (int mark : marks) {
+        sum += mark;
+    }
+    
+    // Calculate the average of individual marks
+    float averageMarks = sum / marks.size();
+    
+    // Calculate the final mark using the formula: 0.4 * average marks + 0.6 * exam
+    return 0.4 * averageMarks + 0.6 * student.get_examMark();
+}
+
+// Function to calculate the final mark using median
+float calculateFinalMarkMed(const Student &student) {
+    // Retrieve the marks using the getter function
+    vector<int> marks = student.get_marks();
+    
+    if (marks.empty()) {
+        return 0.6 * student.get_examMark(); // If no individual marks, return 0.6 * exam mark
+    }
+
+    // Sort the individual marks
+    vector<int> sortedMarks = marks;
+    sort(sortedMarks.begin(), sortedMarks.end());
+    
+    int n = marks.size();
+
+    // Calculate the median
+    float median;
+    if (n % 2 == 0) {
+        // If even number of marks, take the average of the middle two
+        int middle1 = sortedMarks[(n / 2) - 1];
+        int middle2 = sortedMarks[n / 2];
+        median = (static_cast<float>(middle1) + static_cast<float>(middle2)) / 2.0;
+    } else {
+        // If odd number of marks, take the middle value
+        median = static_cast<float>(sortedMarks[n / 2]);
+    }
+
+    // Calculate the final mark using the formula: 0.4 * median + 0.6 * exam
+    return 0.4 * median + 0.6 * student.get_examMark();
+}
 
 
-
-
+// OLD
 int usersChoice() {
     int n;
     printf("Hello! Please, choose of the following options: \n");
@@ -160,53 +213,53 @@ void getInputAuto(Student &student)
 }
 
 // Function to calculate the final mark for a student
-float calculateFinalMarkAvg(const Student &student) {
-    float sum = 0.0;
+// float calculateFinalMarkAvg(const Student &student) {
+//     float sum = 0.0;
 
-    if (student.marks.empty()) {
-        return 0.6 * student.examMark; // If no individual marks, return 0.6 * exam mark
-    }
+//     if (student.marks.empty()) {
+//         return 0.6 * student.examMark; // If no individual marks, return 0.6 * exam mark
+//     }
     
-    // Calculate the sum of individual marks
-    for (int mark : student.marks) {
-        sum += mark;
-    }
+//     // Calculate the sum of individual marks
+//     for (int mark : student.marks) {
+//         sum += mark;
+//     }
     
-    // Calculate the average of individual marks
-    float averageMarks = sum / student.marks.size();
+//     // Calculate the average of individual marks
+//     float averageMarks = sum / student.marks.size();
     
-    // Calculate the final mark using the formula: 0.4 * average marks + 0.6 * exam
-    return 0.4 * averageMarks + 0.6 * student.examMark;
-}
+//     // Calculate the final mark using the formula: 0.4 * average marks + 0.6 * exam
+//     return 0.4 * averageMarks + 0.6 * student.examMark;
+// }
 
 
 // Function to calculate the final mark using median
-float calculateFinalMarkMed(const Student &student) {
-    if (student.marks.empty()) {
-        return 0.6 * student.examMark; // If no individual marks, return 0.6 * exam mark
-    }
+// float calculateFinalMarkMed(const Student &student) {
+//     if (student.marks.empty()) {
+//         return 0.6 * student.examMark; // If no individual marks, return 0.6 * exam mark
+//     }
 
-    // Sort the individual marks
-    vector<int> sortedMarks = student.marks;
-    sort(sortedMarks.begin(), sortedMarks.end());
+//     // Sort the individual marks
+//     vector<int> sortedMarks = student.marks;
+//     sort(sortedMarks.begin(), sortedMarks.end());
     
-    int n = student.marks.size();
+//     int n = student.marks.size();
 
-    // Calculate the median
-    float median;
-    if (n % 2 == 0) {
-        // If even number of marks, take the average of the middle two
-        int middle1 = sortedMarks[(n / 2) - 1];
-        int middle2 = sortedMarks[n / 2];
-        median = (static_cast<float>(middle1) + static_cast<float>(middle2)) / 2.0;
-    } else {
-        // If odd number of marks, take the middle value
-        median = static_cast<float>(sortedMarks[n / 2]);
-    }
+//     // Calculate the median
+//     float median;
+//     if (n % 2 == 0) {
+//         // If even number of marks, take the average of the middle two
+//         int middle1 = sortedMarks[(n / 2) - 1];
+//         int middle2 = sortedMarks[n / 2];
+//         median = (static_cast<float>(middle1) + static_cast<float>(middle2)) / 2.0;
+//     } else {
+//         // If odd number of marks, take the middle value
+//         median = static_cast<float>(sortedMarks[n / 2]);
+//     }
 
-    // Calculate the final mark using the formula: 0.4 * median + 0.6 * exam
-    return 0.4 * median + 0.6 * student.examMark;
-}
+//     // Calculate the final mark using the formula: 0.4 * median + 0.6 * exam
+//     return 0.4 * median + 0.6 * student.examMark;
+// }
 
 void printStudentTable(const vector<Student> &students)
 {
