09732461c2e0ef99f059d026bef7312a5d3a613f
erikakr <erika.kriksciunaite@nordsec.com>
2023-12-28 13:03:00 +0200

    Modified Class Person

--- person.h
+++ person.h
@@ -3,11 +3,28 @@
 
 #include <string>
 
+using std::string;
+
 class Person {
+private:
+    string name;
+    string surname;   
 public:
+    // Constructor
+    // Person(const std::string& name, std::string& surname) : name(name), surname(surname) {}
+    
     // virtual functions 
     virtual std::string get_name() const = 0;
     virtual std::string get_surname() const = 0;
+
+    // Setter methods
+    void set_name(const std::string& name) {
+        this->name = name;
+    }
+    void set_surname(const std::string& surname) {
+        this->surname = surname;
+    }
+
     
     // Virtual destructor (recommended when dealing with inheritance)
     virtual ~Person() {}
